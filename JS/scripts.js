948bd5d02dc3ba0a29f4c57a360d523e19150ed6
Name-Javi <jsolis83014@gmail.com>
2020-11-30 17:34:55 -0600

    Lists!
    Added the ability to list and label smaller and larger pokemon.

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,7 +2,7 @@ let pokemonList = [
     {
     name: 'Bulbasaur', 
     height: 0.7,
-     type: ["grass"],
+    type: ["grass"],
     },
 {
     name: 'Charmander',
@@ -13,6 +13,12 @@ let pokemonList = [
 {
     name: 'Squirtle',
     height: 0.5,
-    type: ["water"]
+    type: ["water"],
 }
 ]
+var i;
+//Used a ternary operator here to find and label the biggest pokemon and the smallest
+for (i = 0; i < pokemonList.length; i++) {
+    let specialText = pokemonList[i].height > 0.6 ? "This is the biggest!" : "This is smaller..."; 
+    document.write(pokemonList[i].name + " " + "(Height:" + pokemonList[i].height + ")" + specialText + "<br>");
+}
